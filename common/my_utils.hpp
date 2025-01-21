86b423178f5d03002eed10d7841666444276afaf
root <root@a0d7b954-ssh.local.hass.io>
2025-01-21 22:26:14 +0100

    Complete re-work of message handling
    * add more helper functions to my_utils.hpp
    * add message and sensor handling to my_messages.hpp
      every sensor now has a name, icon and text (with ID suffix .name and .icon)
      simplify a label in a grid

--- common/my_utils.hpp
+++ common/my_utils.hpp
@@ -1,6 +1,10 @@
 #ifndef MY_UTIL_H
 #define MY_UTIL_H
 
+#include <memory>
+#include <string>
+#include <stdexcept>
+
 // split string into vector
 std::vector<std::string> split(const std::string str, std::string token);
 
@@ -32,6 +36,23 @@ std::vector<std::string> split(std::string str, const std::string token)
     return result;
 }
 
+std::vector<std::string> split_trim(std::string str, const std::string token)
+{
+    std::vector<std::string>result;
+    while (str.size()) {
+        int index = str.find(token);
+        if (index != std::string::npos){
+            result.push_back(trim(str.substr(0,index)));
+            str = str.substr(index + token.size());
+            if (str.size() == 0) result.push_back(str);
+        } else {
+            result.push_back(trim(str));
+            str = "";
+        }
+    }
+    return result;
+}
+
 std::string trim(const std::string& str, const std::string& whitespace)
 {
     const auto strBegin = str.find_first_not_of(whitespace);
@@ -56,9 +77,43 @@ const V & GetWithDef(const std::map <K,V>& m, const K& key, const V& defval)
    }
 }
 
+bool startsWith(std::string text, std::string cmp)
+{
+   return (text.rfind(cmp, 0) == 0);
+}
+
+bool endsWith(std::string text, std::string cmp)
+{
+    if (text.length() >= cmp.length()) {
+        return (0 == text.compare (text.length() - cmp.length(), cmp.length(), cmp));
+    } else {
+        return false;
+    }    
+}
+
 float StringToFloat(const std::string str)
 {
     return atof(str.c_str());
+}
+
+int StringToInt(const std::string str)
+{
+    return atoi(str.c_str());
+}
+
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+template<typename ... Args>
+std::string string_format(const std::string& format, Args ... args)
+{
+    int size_s = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+    if (size_s <= 0) { return ""; }
+    auto size = static_cast<size_t>(size_s);
+    std::unique_ptr<char[]> buf (new char[size]);
+    std::snprintf(buf.get(), size, format.c_str(), args ...);
+    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 }
 
 #endif // MY_UTIL_H
---
+++ lcd/messages/my_messages.hpp
@@ -0,0 +1,124 @@
+#ifndef MY_MESSAGES_H
+#define MY_MESSAGES_H
+
+// color constants
+
+static const lv_color_t red    = lv_color_make(255, 0, 0);
+static const lv_color_t green  = lv_color_make(0, 255, 0);
+static const lv_color_t blue   = lv_color_make(0, 0, 255);
+static const lv_color_t white  = lv_color_make(255, 255, 255);
+static const lv_color_t yellow = lv_color_make(255, 255, 0);
+static const lv_color_t purple = lv_color_make(255, 0, 255);
+static const lv_color_t cyan   = lv_color_make(0, 255, 255);
+static const lv_color_t black  = lv_color_make(0, 0, 0);
+static const lv_color_t gray   = lv_color_make(128, 128, 128);
+
+
+/// Notifications (mapping from string to label)
+
+std::map<std::string, std::list<lv_obj_t *>> allNotifications = {};
+
+lv_obj_tree_walk_res_t _notif_initCallback(lv_obj_t *obj, void *userData) {
+    if (lv_obj_check_type(obj, &lv_label_class)) {
+        lv_label_t *label = reinterpret_cast<lv_label_t *>(obj);
+        if (label->text[0] == '#') {
+            allNotifications[label->text + 1].push_back(obj);
+            lv_label_set_text(obj, " ");
+        }
+    }
+    return LV_OBJ_TREE_WALK_NEXT;
+}
+
+void notif_initialize(lv_obj_t *root) {
+    lv_obj_tree_walk(root, _notif_initCallback, nullptr);
+}
+
+void notif_initialize(lvgl::LvPageType *page) {
+    lv_obj_tree_walk(page->obj, _notif_initCallback, nullptr);
+}
+
+void notif_set_text(std::string id, std::string text, lv_color_t color = white) {
+    for (auto label: allNotifications[id]) {
+        lv_label_set_text(label, text.c_str());
+        lv_obj_set_style_text_color(label, color, LV_PART_MAIN);
+    }
+}
+
+void notif_set_color(std::string id, lv_color_t color) {
+    for (auto label: allNotifications[id]) {
+        lv_obj_set_style_text_color(label, color, LV_PART_MAIN);
+    }
+}
+
+void notif_set(std::string id, std::string name, std::string icon, std::string text, lv_color_t color = white) {
+    notif_set_text(id, text, color);
+    notif_set_text(id+".name", name, color);
+    notif_set_text(id+".icon", icon);
+}
+
+
+/// Warning messages (shown sorted in the message_card)
+
+struct DisplayMessage
+{
+    int Compare(const DisplayMessage &dm) const {
+        return
+            (priority > dm.priority)    ? -1:
+            (priority < dm.priority)    ?  1:
+            (id       > dm.id)          ?  1:
+            (id       < dm.id)          ? -1:
+            (pos      > dm.pos)         ?  1:
+            (pos      < dm.pos)         ? -1:
+                                           0;
+    }
+
+    bool operator == (const DisplayMessage& dm) const {
+        return !Compare(dm);
+    }
+
+    bool operator < (const DisplayMessage& dm) const {
+        return Compare(dm) < 0;   
+    }
+
+    std::string id = "";
+    std::string text = "";
+    std::string icon = "";
+    int priority = 0;
+    int pos = 0;
+};
+std::map<std::string, std::list<DisplayMessage>> allMessages = {};
+
+void warn_clear(const std::string id)
+{
+    allMessages[id].clear();
+}
+
+void warn_add(const std::string id, const std::string &text, const std::string &icon, const int priority)
+{
+    DisplayMessage m;
+    m.id = id;
+    m.text = text;
+    m.icon = icon;
+    m.priority = priority;
+    m.pos = allMessages[id].size();
+    allMessages[id].push_back(m);
+}
+
+void warn_set(const std::string id, const std::string &text, const std::string &icon, const int priority)
+{
+    warn_clear(id);
+    warn_add(id, text, icon, priority);
+}
+
+std::multiset<DisplayMessage> warn_get_sorted()
+{
+    std::multiset<DisplayMessage> ret;
+    for (auto ldm: allMessages) {
+        for (auto dm: ldm.second) {
+            ret.insert(dm);
+        }
+    }
+    return ret;
+}
+
+#endif // MY_MESSGES_H
